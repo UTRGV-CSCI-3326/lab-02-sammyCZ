95b67b5febb5d918a1479d88195189425628cb3b
sammyCZ <161363192+sammyCZ@users.noreply.github.com>
2025-02-02 19:14:56 +0000

    Commit 3

--- src/MixMatch.java
+++ src/MixMatch.java
@@ -1,5 +1,5 @@
 public class MixMatch {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         
         String name = "Sam";
         System.out.println(name + ": String is for a sequence of characters");
